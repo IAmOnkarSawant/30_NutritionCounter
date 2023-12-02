cb837186083c317b9a9eaeeb56f8ee033c012ec4
NithinV-tech <nitinsachin26@gmail.com>
2023-12-02 14:47:38 +0530

    "form added"

--- frontend/src/pages/Table.js
+++ frontend/src/pages/Table.js
@@ -6,6 +6,10 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 // Add these imports
 import Card from '@mui/material/Card';
@@ -23,6 +27,12 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+// import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress
+
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
 
 // Define your theme or use an existing one
 const defaultTheme = createTheme();
@@ -34,20 +44,18 @@ const PrettoSlider = styled(Slider)(({ theme }) => ({
   // ... (other styles)
 }));
 
+
+
 // Default values for different nutrients
 const data = {
-  Calorie: 'medium',
-  Fats: 'low',
-  Protein: 'high',
+  Calorie: 'high',
+  Fats: 'medium',
+  Protein: 'medium',
   Sodium: 'medium',
-  Carbohydrate: 'low',
+  Carbohydrate: 'medium',
 };
 
 export default function Table() {
-  const handleChange = (key, value) => {
-    console.log(`Setting ${key} to ${value}`);
-  };
-
   const [initialLoad, setInitialLoad] = useState(true);
   const [userInfo, setUserInfo] = useState({
     age: '',
@@ -56,6 +64,7 @@ export default function Table() {
     gender: '',
   });
   const [openDialog, setOpenDialog] = useState(true);
+  const [bmi, setBMI] = useState(null);
 
   useEffect(() => {
     // Display the pop-up on initial load
@@ -71,10 +80,13 @@ export default function Table() {
 
   const handleDialogSubmit = () => {
     // Perform BMI calculation here based on userInfo
-    const bmi = calculateBMI(userInfo.weight, userInfo.height);
+    const calculatedBMI = calculateBMI(userInfo.weight, userInfo.height);
+
+    // Set the BMI to be displayed
+    setBMI(calculatedBMI);
 
     // Send BMI to the backend or perform other actions as needed
-    sendBMItoBackend(bmi);
+    sendBMItoBackend(calculatedBMI);
 
     // Reset the user info
     setUserInfo({
@@ -92,13 +104,18 @@ export default function Table() {
     // Formula: BMI = weight (kg) / (height (m))^2
     const weightInKg = parseFloat(weight);
     const heightInM = parseFloat(height) / 100; // Convert height to meters
-    return weightInKg / (heightInM * heightInM);
+    return weightInKg / (heightInM * heightInM); // Multiply by 100 to get a percentage
   };
 
   const sendBMItoBackend = (bmi) => {
     // Send BMI data to the backend here
     // Example: fetch('/api/bmi', { method: 'POST', body: JSON.stringify({ bmi }), headers: { 'Content-Type': 'application/json' } })
     console.log('BMI sent to the backend:', bmi);
+  };
+
+  const handleChange = (key, value) => {
+    console.log(`Setting ${key} to ${value}`);
+    // You can add logic here to handle the changes in nutrient values
   };
 
   return (
@@ -110,7 +127,45 @@ export default function Table() {
         </Toolbar>
       </AppBar>
       <main>
-        <Container sx={{ py: 8, background: 'non' }} maxWidth="md">
+        <Container sx={{ py: 8, background: 'none' }} maxWidth="md">
+          {/* Display small-sized BMI */}
+          {bmi !== null && (
+            <Box sx={{ textAlign: 'center', mb: 4 }}>
+              {bmi !== undefined && (
+                <CircularProgressbar
+                  // 
+                  value={bmi}
+                  text={`${bmi.toFixed(2)}`}
+                  styles={buildStyles({
+                    // Rotation of path and trail, in number of turns (0-1)
+                    rotation: 0.25,
+
+                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                    strokeLinecap: 'butt',
+
+                    // Text size
+                    textSize: '16px',
+
+                    // How long animation takes to go from one percentage to another, in seconds
+                    pathTransitionDuration: 0.5,
+
+                    // Can specify path transition in more detail, or remove it entirely
+                    // pathTransition: 'none',
+
+                    // Colors
+                    pathColor: `rgba(62, 152, 199, ${bmi / 100})`,
+                    textColor: '#f88',
+                    trailColor: '#d6d6d6',
+                    backgroundColor: '#3e98c7',
+                  })}
+                />
+              )}
+              <Typography variant="body2" color="textSecondary">
+                {`${bmi.toFixed(2)}`}
+              </Typography>
+            </Box>
+          )}
+
           {!openDialog && (
             <Grid container spacing={4}>
               {Object.entries(data).map(([key, value]) => (
@@ -120,7 +175,6 @@ export default function Table() {
                       <Typography gutterBottom variant="h5" component="h2">
                         {key}
                       </Typography>
-                      {/* Use the styled PrettoSlider component here */}
                       <PrettoSliderCard
                         value={value}
                         onChange={(newValue) => handleChange(key, newValue)}
@@ -164,22 +218,21 @@ export default function Table() {
             margin="normal"
           />
           {/* Updated InputLabel and Select for Gender */}
-          {userInfo.gender === '' && (
-            <TextField
-              label="Gender"
+          <TextField
+            label="Gender"
+            fullWidth
+            margin="normal"
+          ></TextField>
+            <Select
               value={userInfo.gender}
               onChange={(e) => setUserInfo({ ...userInfo, gender: e.target.value })}
-              fullWidth
-              select
-              margin="normal"
             >
-              <option value="" disabled>
-                Select Gender
-              </option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </TextField>
-          )}
+              <MenuItem value="">Select Gender</MenuItem>
+              <MenuItem value="male">Male</MenuItem>
+              <MenuItem value="female">Female</MenuItem>
+            </Select>
+          
+
         </DialogContent>
         <DialogActions>
           <Button
@@ -209,25 +262,34 @@ function PrettoSliderCard({ value, onChange, initialLoad }) {
     // Move the slider to its respective position after a delay
     if (!initialLoad) {
       const delay = setTimeout(() => {
-        setSliderValue(value === 'low' ? 0 : value === 'medium' ? 50 : 100);
-      }, 2000); // Adjust the delay as needed
+        setSliderValue(value === 'low' ? 0 : value === 'medium' ? 0 : 0);
+      }, 100); // Adjust the delay as needed
 
-      return () => clearTimeout(delay);
+      const resetDelay = setTimeout(() => {
+        setSliderValue(value === 'low' ? 0 : value === 'medium' ? 50 : 100);
+      }, 3000); // Adjust the delay as needed
+
+      return () => {
+        clearTimeout(delay);
+        clearTimeout(resetDelay);
+      };
     }
   }, [initialLoad, value]);
+
+  const handleChange = (event, newValue) => {
+    setSliderValue(newValue);
+    onChange(newValue);
+  };
 
   return (
     <div>
       {/* Use the styled PrettoSlider component here */}
       <PrettoSlider
         value={sliderValue}
-        onChange={(event, newValue) => {
-          setSliderValue(newValue);
-          onChange(newValue);
-        }}
+        onChange={handleChange}
         aria-labelledby="discrete-slider"
         valueLabelDisplay="auto"
-        step={50}
+        step={1} // Adjust the step for smoother movement
         marks={[
           { value: 0, label: 'Low' },
           { value: 50, label: 'Medium' },
@@ -236,7 +298,7 @@ function PrettoSliderCard({ value, onChange, initialLoad }) {
         min={0}
         max={100}
         sx={{
-          transition: 'all 1s ease-in-out', // Add a transition for smoother movement
+          transition: 'all 2s ease-in-out', // Add a transition for smoother movement
         }}
       />
     </div>
