ecf75e3559a1a417abbd01914d71ac843ae40d58
IAmOnkarSawant <onkarsawant5613@gmail.com>
2024-10-29 15:52:18 +0530

    "Minor fixes"

--- Backend/Routes/home.js
+++ Backend/Routes/home.js
@@ -4,68 +4,95 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 
-const Questions = require("../Models/questionsModel");
 const Food = require("../Models/foodModel");
-const UserDet = require("../Models/userModel");
 const NoPregnancy = require("../Models/NoPregnancyModel");
 const NoCardiac = require("../Models/NoCadiacModel");
 const NoChild = require("../Models/NoChildModel");
 const NoDiabetic = require("../Models/NoDiabeticModel");
-const requireAuth = require("../Middleware/requireAuth");
-// const User = require("../Models/userInfoModel");
+
 const router = express.Router();
+
+// Local variables
 let nutrients = [];
 let table_contents = [];
 let user_details = [];
 let nutrients_details = [];
 
 //==========================================================================//
-//                           Table Routes                                   //
+//                       Upload File Routes                                 //
 //==========================================================================//
 
 //------------------------------------------------------------
-// Post the BMI details of user
+// Upload the file
 //------------------------------------------------------------
-router.post("/post-user-bmi", async (req, res) => {
-  // try {
-  //   const { age, weight, height, gender, bmi } = req.body;
-  //   // Validate the incoming data (you may want to add more validation)
-  //   if (!age || !weight || !height || !gender || !bmi) {
-  //     return res.status(400).json({ error: 'Incomplete data' });
-  //   }
-  //   try {
-  //     // Clear all documents from the newUser collection
-  //     await User.deleteMany({});
-  //     console.log('Collection cleared');
-  //   } catch (error) {
-  //     console.error('Error clearing collection:', error);
-  //   }
-  //   // Assuming you have a MongoDB model for user data, replace 'User' with your actual model
-  //   const newUser = new User({
-  //     age,
-  //     weight,
-  //     height,
-  //     gender,
-  //     bmi,
-  //   });
-  //   // Save the user data to your database
-  //   await newUser.save();
-  //   res.status(201).json({ message: 'User BMI data saved successfully' });
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ error: 'Internal Server Error' });
-  // }
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+});
+
+router.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+router.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const uploadedFile = req.file;
+  const filePath = uploadedFile.path;
+  nutrients.splice(0, nutrients.length);
+  table_contents.splice(0, table_contents.length);
+  user_details.splice(0, user_details.length);
+  nutrients_details.splice(0, nutrients_details.length);
+
+  res
+    .status(200)
+    .json({ message: "File uploaded successfully", filePath: filePath });
 });
 
 //------------------------------------------------------------
-// Run the model for table
+// clear uploads folder
 //------------------------------------------------------------
-router.get("/run-python-script-table", (req, res) => {
+const parentDirectory = path.join(__dirname, "..");
+const folderPath = path.join(parentDirectory, "uploads");
+router.get("/delete-files", (req, res) => {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err}`);
+      res.status(500).send("Unable to read the directory.");
+      return;
+    }
+
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Error deleting file: ${err}`);
+        } else {
+          console.log(`Deleted file: ${filePath}`);
+        }
+      });
+    });
+    res.send("All files in the folder have been deleted.");
+  });
+});
+
+//------------------------------------------------------------
+// Run the model for neutrients
+//------------------------------------------------------------
+router.get("/run-python-script", (req, res) => {
   let stdout = "";
   let stderr = "";
 
   console.log(__dirname);
-  const pythonScriptPath = path.resolve(__dirname, "modelTable.py");
+  const pythonScriptPath = path.resolve(__dirname, "model.py");
   const pythonScriptCommand = `python "${pythonScriptPath}"`;
   console.log(pythonScriptCommand);
   const pythonProcess = exec(pythonScriptCommand);
@@ -79,31 +106,22 @@ router.get("/run-python-script-table", (req, res) => {
   });
 
   pythonProcess.on("close", (code) => {
-    let response;
     if (code !== 0) {
       console.error(`Error: Python script exited with code ${code}`);
-      const errorOutput = stderr.split("\n").filter(Boolean);
-      response = { success: false, error: `Python script exited with code ${code}`, errorDetails: errorOutput };
-    } else {
-      try {
-        const noutput = JSON.parse(stdout);
-        const errorOutput = stderr.split("\n").filter(Boolean);
-        for (const table_content of noutput) {
-          if (table_content["percentage"] > 20) table_content["DV%"] = "high";
-          else if (table_content["percentage"] >= 5 && table_content["percentage"] <= 20)
-            table_content["DV%"] = "normal";
-          else table_content["DV%"] = "low";
-        }
-        response = { success: true, data: noutput, errorOutput };
-      } catch (e) {
-        console.error(`Error: Failed to parse Python script output - ${e}`);
-        response = { success: false, error: `Failed to parse Python script output`, errorDetails: e.toString() };
-      }
     }
-    res.json(response);
+
+    const output = stdout.split("\n").filter(Boolean);
+    const noutput = output.map((element) => element.replace(/\r/g, ""));
+    const errorOutput = stderr.split("\n").filter(Boolean);
+    nutrients = noutput;
+    res.json({ noutput, errorOutput });
   });
 });
 module.exports = router;
+
+//==========================================================================//
+//                           Model Routes                                   //
+//==========================================================================//
 
 //------------------------------------------------------------
 // Run the model for combined
@@ -152,95 +170,6 @@ router.get("/run-python-script-combined", (req, res) => {
   });
 });
 module.exports = router;
-//==========================================================================//
-//                       Neutrients Routes                                  //
-//==========================================================================//
-
-//==========================================================================//
-//                           Table Routes                                   //
-//==========================================================================//
-
-//------------------------------------------------------------
-// Post the BMI details of user
-//------------------------------------------------------------
-router.post("/post-user-bmi", async (req, res) => {
-  // try {
-  //   const { age, weight, height, gender, bmi } = req.body;
-  //   // Validate the incoming data (you may want to add more validation)
-  //   if (!age || !weight || !height || !gender || !bmi) {
-  //     return res.status(400).json({ error: 'Incomplete data' });
-  //   }
-  //   try {
-  //     // Clear all documents from the newUser collection
-  //     await User.deleteMany({});
-  //     console.log('Collection cleared');
-  //   } catch (error) {
-  //     console.error('Error clearing collection:', error);
-  //   }
-  //   // Assuming you have a MongoDB model for user data, replace 'User' with your actual model
-  //   const newUser = new User({
-  //     age,
-  //     weight,
-  //     height,
-  //     gender,
-  //     bmi,
-  //   });
-  //   // Save the user data to your database
-  //   await newUser.save();
-  //   res.status(201).json({ message: 'User BMI data saved successfully' });
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(500).json({ error: 'Internal Server Error' });
-  // }
-});
-
-//------------------------------------------------------------
-// Run the model for table
-//------------------------------------------------------------
-router.get("/run-python-script/table", (req, res) => {
-  let stdout = "";
-  let stderr = "";
-
-  console.log(__dirname);
-  const pythonScriptPath = path.resolve(__dirname, "modelTable.py");
-  const pythonScriptCommand = `python "${pythonScriptPath}"`;
-  console.log(pythonScriptCommand);
-  const pythonProcess = exec(pythonScriptCommand);
-
-  pythonProcess.stdout.on("data", (data) => {
-    stdout += data;
-  });
-
-  pythonProcess.stderr.on("data", (data) => {
-    stderr += data;
-  });
-
-  pythonProcess.on("close", (code) => {
-    if (code !== 0) {
-      console.error(`Error: Python script exited with code ${code}`);
-    }
-
-    // Processing the Python script's output and stderr
-    const noutput = JSON.parse(stdout);
-    const errorOutput = stderr.split("\n").filter(Boolean);
-    table_contents = noutput;
-    for (const table_content of table_contents) {
-      if (table_content["percentage"] > 20) table_content["DV%"] = "high";
-      else if (
-        table_content["percentage"] >= 5 &&
-        table_content["percentage"] <= 20
-      )
-        table_content["DV%"] = "normal";
-      else table_content["DV%"] = "low";
-    }
-    res.json({ noutput, errorOutput });
-  });
-});
-module.exports = router;
-
-//==========================================================================//
-//                       Neutrients Routes                                  //
-//==========================================================================//
 
 //==========================================================================//
 //                           Table Routes                                   //
@@ -301,20 +230,17 @@ router.get("/run-python-script/table", (req, res) => {
     if (code !== 0) {
       console.error(`Error: Python script exited with code ${code}`);
     }
-
-    // Process the Python script's output and stderr
-
-    // const output = stdout.split("\n").filter(Boolean);
-    // const noutput = output.map((element) => element.replace(/\r/g, ""));
-    // const errorOutput = stderr.split("\n").filter(Boolean);
-    // nutrients = noutput;
+    const output = stdout.split("\n").filter(Boolean);
+    const noutput = output.map((element) => element.replace(/\r/g, ""));
+    const errorOutput = stderr.split("\n").filter(Boolean);
+    nutrients = noutput;
     res.json({ noutput, errorOutput });
   });
 });
 module.exports = router;
 
 //==========================================================================//
-//                       Neutrients Routes                                  //
+//                       Nutrients Routes                                   //
 //==========================================================================//
 //------------------------------------------------------------
 // Should I reccomend to pregnent?
@@ -606,102 +532,3 @@ router.get("/get-nutrients", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-//------------------------------------------------------------
-// Upload the file
-//------------------------------------------------------------
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
-});
-
-router.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-router.post("/upload", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
-
-  const uploadedFile = req.file;
-  const filePath = uploadedFile.path;
-  nutrients.splice(0, nutrients.length);
-  table_contents.splice(0, table_contents.length);
-  user_details.splice(0, user_details.length);
-  nutrients_details.splice(0, nutrients_details.length);
-
-  res
-    .status(200)
-    .json({ message: "File uploaded successfully", filePath: filePath });
-});
-
-//------------------------------------------------------------
-// clear uploads folder
-//------------------------------------------------------------
-const parentDirectory = path.join(__dirname, "..");
-const folderPath = path.join(parentDirectory, "uploads");
-router.get("/delete-files", (req, res) => {
-  fs.readdir(folderPath, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory: ${err}`);
-      res.status(500).send("Unable to read the directory.");
-      return;
-    }
-
-    files.forEach((file) => {
-      const filePath = path.join(folderPath, file);
-
-      // Delete the file
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          console.error(`Error deleting file: ${err}`);
-        } else {
-          console.log(`Deleted file: ${filePath}`);
-        }
-      });
-    });
-    res.send("All files in the folder have been deleted.");
-  });
-});
-
-//------------------------------------------------------------
-// Run the model for neutrients
-//------------------------------------------------------------
-router.get("/run-python-script", (req, res) => {
-  let stdout = "";
-  let stderr = "";
-
-  console.log(__dirname);
-  const pythonScriptPath = path.resolve(__dirname, "model.py");
-  const pythonScriptCommand = `python "${pythonScriptPath}"`;
-  console.log(pythonScriptCommand);
-  const pythonProcess = exec(pythonScriptCommand);
-
-  pythonProcess.stdout.on("data", (data) => {
-    stdout += data;
-  });
-
-  pythonProcess.stderr.on("data", (data) => {
-    stderr += data;
-  });
-
-  pythonProcess.on("close", (code) => {
-    if (code !== 0) {
-      console.error(`Error: Python script exited with code ${code}`);
-    }
-
-    // Process the Python script's output and stderr
-    const output = stdout.split("\n").filter(Boolean);
-    const noutput = output.map((element) => element.replace(/\r/g, ""));
-    const errorOutput = stderr.split("\n").filter(Boolean);
-    nutrients = noutput;
-    res.json({ noutput, errorOutput });
-  });
-});
-module.exports = router;
